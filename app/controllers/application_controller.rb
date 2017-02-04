c7fd4d7b63a333f190d2698cdf59c3e6ab1ce729
kiosk <kio.watanabe.1005@gmail.com>
2017-02-04 16:31:35 +0900

    全部リモートにあげます

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,30 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+   protected
+
+ 	def edit
+		super
+　	end
+
+	def update
+		super
+	end
+
+	private
+
+	# def configure_permitted_parameters
+	#   devise_parameter_sanitizer.for(:account_update) do |u|
+	#     u.permit(:name, :email, :password, :password_confirmation, :current_password, :image)
+	#   end
+	# end
+	def configure_permitted_parameters
+    added_attrs = [ :username, :email, :password, :password_confirmation　]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
+  end
+end
---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,84 @@
+class PhotosController < ApplicationController
+  before_action :login_check, only: [:new, :edit, :update, :destroy]
+  before_action :set_current_user_photo, only: [:edit, :update, :destroy]
+
+  # GET /photos
+  # GET /photos.json
+  def index
+    @photos = Photo.all
+  end
+
+  # GET /photos/1
+  # GET /photos/1.json
+  def show
+    @photo = Photo.includes(:user).find(params[:id])
+    @comments = @photo.comments.includes(:user).all
+    @comment  = @photo.comments.build(user_id: current_user.id) if current_user
+  end
+
+  # GET /photos/new
+  def new
+     @photo = current_user.photos.build
+  end
+
+  # GET /photos/1/edit
+  def edit
+  end
+
+  # POST /photos
+  # POST /photos.json
+  def create
+    @photo = Photo.new(photo_params)
+
+    respond_to do |format|
+      if @photo.save
+        format.html { redirect_to @photo, notice: '画像の投稿が完了しました。' }
+        format.json { render :show, status: :created, location: @photo }
+      else
+        format.html { render :new }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /photos/1
+  # PATCH/PUT /photos/1.json
+  def update
+    respond_to do |format|
+      if @photo.update(photo_params)
+        format.html { redirect_to @photo, notice: '画像の編集が完了しました。' }
+        format.json { render :show, status: :ok, location: @photo }
+      else
+        format.html { render :edit }
+        format.json { render json: @photo.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /photos/1
+  # DELETE /photos/1.json
+  def destroy
+    @photo.destroy
+    respond_to do |format|
+      format.html { redirect_to photos_url, notice: '画像の消去が完了しました。' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def login_check
+      unless user_signed_in?
+        flash[:alert] = "ログインしてください"
+        redirect_to root_path
+      end
+    end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_current_user_photo
+      @photo = current_user.photos.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def photo_params
+       params.require(:photo).permit(:image, :caption, :user_id)
+    end
+end
