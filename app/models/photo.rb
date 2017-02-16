8b6232c9ff93ea0a3caf302542b75fd3a2e1a1c6
kiosk <kio.watanabe.1005@gmail.com>
2017-02-16 14:44:13 +0900

    ミス消去
    ミス部分のコードの消去を実行

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,6 @@ class Photo < ActiveRecord::Base
 	 belongs_to :user
 	 has_many :comments
 	 
-	 acts_as_taggable_on :labels # post.label_list が追加される
+	acts_as_taggable_on :labels # post.label_list が追加される
   	acts_as_taggable            # acts_as_taggable_on :tags のエイリアス
-  	  Photo.order("created_at DESC")
 end
