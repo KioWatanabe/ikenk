d99f7db5cfd2e57c10f2cc2ec621db8aab746890
kiosk <kio.watanabe.1005@gmail.com>
2017-02-16 14:24:47 +0900

    投稿順に並び替え
    投稿された画像を投稿順に並べ替えるための処理を実行

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  
+
    protected
 
  	def edit
@@ -17,7 +19,6 @@ class ApplicationController < ActionController::Base
 	def after_sign_in_path_for(resource)
 	  photos_path
 	end
-
 
 
 	private
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -5,4 +5,5 @@ class Photo < ActiveRecord::Base
 	 
 	 acts_as_taggable_on :labels # post.label_list が追加される
   	acts_as_taggable            # acts_as_taggable_on :tags のエイリアス
+  	  Photo.order("created_at DESC")
 end
